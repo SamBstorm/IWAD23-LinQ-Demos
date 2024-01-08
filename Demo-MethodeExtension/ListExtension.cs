7d4ecad4886951195c4a660baf7ca7d1c55957f0
Samuel Legrain <samuel.legrain@bstorm.be>
2024-01-08 11:34:28 +0100

    Add Démo Méthodes Extensions

---
+++ Demo-MethodeExtension/ListExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MethodeExtension
+{
+    internal static class ListExtension
+    {
+        internal static List<T> Filtre<T>(this List<T> list, Func<T, bool> fonctionFiltre)
+        {
+            List<T> result = new List<T>();
+
+            foreach (T item in list)
+            {
+                if (fonctionFiltre(item)) result.Add(item);
+            }
+
+            return result;
+        }
+
+        internal static List<TConvert> Convertir<T,TConvert> (this List<T> list, Func<T, TConvert> fonctionConvertion)
+        {
+            List<TConvert> result = new List<TConvert>();
+            foreach (T item in list)
+            {
+                result.Add(fonctionConvertion(item));
+            }
+            return result;
+        }
+    }
+}
---
+++ Demo-MethodeExtension/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Demo_MethodeExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> entiers = new List<int>() { 2, 4, 5, 9, -3, 7, 8 };
+
+            List<int> result = entiers.Filtre<int>((item) => item % 2 == 0);
+
+            Console.WriteLine(result.Count);
+
+            List<string> stringList = entiers.Convertir<int, string>((i) => i.ToString());
+
+            foreach (string s in stringList)
+            {
+                Console.WriteLine(s);
+            }
+
+            var entiersAvecCarre = entiers.Convertir((i) => new { value = i, square = i * i });
+
+            foreach (var intSquare in entiersAvecCarre)
+            {
+                Console.WriteLine($"Le carré de {intSquare.value} est {intSquare.square}");
+            }
+        }
+    }
+}
